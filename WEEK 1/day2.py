a2308604cd9ffaf3a8546703949421f756d7d212
Naveed <naveed04.ai@gmail.com>
2025-12-18 22:19:13 +0530

    python code for my age after 5 years

---
+++ WEEK 1/day2.py
@@ -0,0 +1,10 @@
+# Day 2 - Python Basics
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("Your age after 5 years will be:", age + 5)
+
+print(type(name))
+print(type(age))
